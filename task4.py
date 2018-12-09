e898fd1c2e353f58ef39a686c2ab4ac4c4e3563f
ViktorKarpilov <wktorion@gmail.com>
2018-12-09 19:06:19 +0200

    finish

--- task4.py
+++ task4.py
@@ -1,16 +1,17 @@
 from data import dataset
 from task1 import *
-
+from task3 import recursionBySeconds
 import plotly
 import plotly.graph_objs as go
 
 
-#Вивести стовпчикову діаграму: хто скільки грошей витратив.
+#Вивести стовпчикову діаграму: на якій трасі ,який найкращий час
+def get_set(raw_set):
+    raw_set = dict(raw_set)
+    for i,j in raw_set.items():
+        raw_set[i] = min(j)
+    return raw_set
 
-data = ?
-
-diagram = ?
-
-fig = ?
-
-plotly.offline.plot(?)
+datas = get_set(recursionBySeconds(dataset))
+data = go.Bar(x=list(datas.keys()),y=list(datas.values()))
+plotly.offline.plot([data])
